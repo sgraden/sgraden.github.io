01775e40f7a8ce5d887f0795a53268bf3c8af50e
sgraden <steven.raden@yahoo.com>
2014-10-11 13:07:00 -0700

    Changed to fullpage.js

--- index.js
+++ index.js
@@ -3,80 +3,21 @@
 
 	//$(draw);
 	$(document).ready( function() {
-
-		/*$(document).keydown(function(e){
-		    //left
-		    if (e.keyCode == 37) { 
-				$("#experience").toggle("slide", {direction:"left"}, 500);
-				navShow();
-		    }
-		    //up
-		    if (e.keyCode == 38) { 
-
-		    }
-		    //right
-		    if (e.keyCode == 39) { 
-				$("#about").toggle("slide", {direction:"right"}, 500);
-				navShow();
-		    }
-		    //down
-		    if (e.keyCode == 40) { 
-		    	$("#gallery").toggle("slide", {direction:"up"}, 500);
-				navShow();
-		    }
-		});*/
-
-		/*About*/
-		// $("#arrow-up").click(function() {
-		// 	$("#about").toggle("slide", {direction:"up"}, 500);
-		// 	navShow();
-		// });
-
-		// $("#about").click(function() {
-		// 	$(this).toggle( "slide", {direction:"up"}, 500);
-		// 	navShow();
-		// });
-
-		// /*Gallery*/
-		// $("#arrow-down").click(function() {
-		// 	$("#gallery").toggle("slide", {direction:"down"}, 500);
-		// 	navShow();
-		// });
-
-		// $("#gallery").click(function() {
-		// 	$(this).toggle( "slide", {direction:"down"}, 500);
-		// 	navShow();
-		// });
-
-		// /*Experience*/
-		// $("#arrow-left").click(function() {
-		// 	$("#experience").toggle("slide", {direction:"left"}, 500);
-		// 	navShow();
-		// });
-
-		// $("#experience").click(function() {
-		// 	$(this).toggle( "slide", {direction:"left"}, 500);
-		// 	navShow();
-		// });
-
-		/*-----
-		$("#arrow-up").click(function() {
-			$("#about").toggle("slide", {direction:"down"}, 500);
-			navShow();
+		$('#fullpage').fullpage({
+			//anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+			sectionsColor: ['#DEDEDE'], //#DEDEDE
+			scrollingSpeed: 300,
+			css3: true,
+			navigation: true,
+			navigationPosition: 'right',
+			navigationTooltips: ['Home', 'About', 'Projects', 'Work', 'Contact']
+			//fixedElements: '#element1, .element2'
+			//menu: true,
+			
 		});
-
-		$("#about").click(function() {
-			$(this).toggle( "slide", {direction:"right"}, 500);
-			navShow();
-		});*/
 	});
 
-	// function navShow() {
-	// 	$("#arrow-up").toggle("slide", {direction: "up"}, 300);
-	// 	$("#arrow-down").toggle("slide", {direction: "down"}, 300);
-	// 	$("#arrow-right").toggle("slide", {direction: "right"}, 300);
-	// 	$("#arrow-left").toggle("slide", {direction: "left"}, 300);
-	// }
+
 
 	
 })();
