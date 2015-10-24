56497b0f31a6ae9b0b8adeb9e997d5fa69d8c44a
Steven Raden <stevengraden@gmail.com>
2015-10-24 11:17:06 -0500

    got react to work slightly

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp  = require('gulp');
+var sass  = require('gulp-sass');
+var react = require('gulp-react');
+
 
 gulp.task('styles', function() {
     gulp.src('scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('react', function() {
+    gulp.src('./build/**/*.js')
+        .pipe(react())
+        .pipe(gulp.dest('./js/'));
 });
 
 //Watch task
 gulp.task('default',function() {
     gulp.watch('scss/**/*.scss',['styles']);
+    gulp.watch('build/myreact.js', ['react']);
 });
