ca211c9286a1a52206466d68348bc4680e5f6c30
Steven Raden <stevengraden@gmail.com>
2015-10-24 23:41:54 -0500

    setting up for reacting the portfolio pieces

--- src/app.js
+++ src/app.js
@@ -1,10 +1,68 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
+/*
+<div class="col s12 m6 l4">
+    <div class="card small hoverable">
 
-var App = React.createClass({
-    render() {
-        return <h1>HWasdfo</h1>;
+        <div class="card-content black-text">
+            <span class="card-title black-text">Card Title</span>
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+            <a href="#">This is a link</a>
+        </div>
+    </div>
+</div>
+*/
+
+var CardAction = React.createClass({
+    render: function() {
+        return (
+            <div className="card-action">
+                <a href="#">This is a link</a>
+            </div>
+        );
     }
 });
 
-ReactDOM.render(<App/>, document.getElementById('example'));
+var CardContent = React.createClass({
+    render: function() {
+        return (
+            <div className="card-content black-text">
+                <span className="card-title black-text">example</span>
+                <p>example</p>
+            </div>
+        );
+    }
+});
+
+var Card = React.createClass({
+    // loadCommentsFromServer: function() {
+    //     $.ajax({
+    //         url: this.props.url,
+    //         dataType: 'json',
+    //         cache: false,
+    //         success: function(data) {
+    //             this.setState({data: data});
+    //         }.bind(this),
+    //             error: function(xhr, status, err) {
+    //             console.error(this.props.url, status, err.toString());
+    //         }.bind(this)
+    //     });
+    // },
+    render: function() {
+        return (
+            <div className="col s12 m6 l4">
+                <div className="card small hoverable">
+                    <CardContent />
+                    <CardAction />
+                </div>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Card />,
+    document.getElementById('portfolio')
+);
