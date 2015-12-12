3cfd0e04343f25af547499ea798305afce3c607f
Steven Raden <stevengraden@gmail.com>
2015-12-12 13:34:42 -0600

    reformatted

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
-var gulp  = require('gulp');
-var sass  = require('gulp-sass');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 var react = require('gulp-react');
-var browserify = require('gulp-browserify');
+// var browserify = require('gulp-browserify');
 
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     gulp.src('scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
     // Single entry point to browserify
     gulp.src('./src/app.js')
         .pipe(react())
         .pipe(browserify({
-          insertGlobals : true,
-          debug : !gulp.env.production
+            insertGlobals: true,
+            debug: !gulp.env.production
         }))
         .pipe(gulp.dest('./dist/'))
 });
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('./scss/**/*.scss',['styles']);
+gulp.task('default', function () {
+    gulp.watch('./scss/**/*.scss', ['styles']);
     gulp.watch('./src/*.js', ['scripts']);
 });
